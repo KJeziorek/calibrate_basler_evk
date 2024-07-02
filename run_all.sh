e44937ae3f39f7e0a54b5bf712283c2907a2fff1
KJeziorek <kjeziorek@student.agh.edu.pl>
2024-07-02 20:22:42 +0200

    bash file changes

--- run_all.sh
+++ run_all.sh
@@ -1,7 +1,9 @@
-num_samples=500
+num_samples_to_reconstruct=200
+num_samples_to_calibrate=50
+time_window=20
 
 export HDF5_PLUGIN_PATH=$HDF5_PLUGIN_PATH:/usr/lib/x86_64-linux-gnu/hdf5/plugins 
 
-python3 reconstruct.py -i events/output.hdf5 --num_samples 100
-python3 calibrate.py --num_samples 50
+python3 reconstruct.py -i events/output.hdf5 --num_samples $num_samples_to_reconstruct -T $time_window
+python3 calibrate.py --num_samples $num_samples_to_calibrate
 python3 visualise.py -i events/output.hdf5
